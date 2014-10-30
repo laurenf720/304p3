c6ba356812d7f8e82bfe650266d9c749b14c52e9
lauren fung <lauren@alumni.ubc.ca>
2014-10-30 14:27:02 -0700

    fixing the foreign key constraints

--- SQLScripts/AMS.sql
+++ SQLScripts/AMS.sql
@@ -2,89 +2,81 @@
 -- USE AMS;
 -- SELECT database();
 
-drop table if exists item;
+DROP TABLE IF EXISTS returnitem;
+DROP TABLE IF EXISTS returns;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS purchaseitem;
+DROP TABLE IF EXISTS purchase; -- changing name of order table to purchase
+DROP TABLE IF EXISTS hassong;
+DROP TABLE IF EXISTS leadsinger;
+DROP TABLE IF EXISTS item;
 
-create table item 
-	(upc 		CHAR(11) not null,
-    title 		CHAR(11) not null,
-    itype 		CHAR(3) not null, 	-- type is a keyword so i put 'i' before it
-    category 	CHAR(12) not null, 	-- based on the length of longest allowed category
-    company 	CHAR(15) not null,
-    iyear 		SMALLINT not null,
-    price		DOUBLE(5,2) not null, 
+CREATE TABLE item 
+	(upc 		CHAR(11) NOT NULL,
+    title 		CHAR(11) NOT NULL,
+    itype 		CHAR(3) NOT NULL, 	-- type is a keyword so i put 'i' before it
+    category 	CHAR(12) NOT NULL, 	-- based on the length of longest allowed category
+    company 	CHAR(15) NOT NULL,
+    iyear 		SMALLINT NOT NULL,
+    price		DOUBLE(5,2) NOT NULL, 
     stock		SMALLINT,
     PRIMARY KEY (upc));
     
-drop table if exists leadsinger;
+CREATE TABLE leadsinger
+	(upc 	CHAR(11) NOT NULL,
+    lsname 	CHAR(15) NOT NULL,
+    PRIMARY KEY (upc, lsname),
+    FOREIGN KEY (upc) REFERENCES item (upc));
 
-create table leadsinger
-	(upc 		CHAR(11) not null,
-    lsname 	CHAR(15) not null,
-    PRIMARY KEY (upc, lsname)
-    -- FOREIGN KEY (upc) REFERENCES item);
-    );
-    
-drop table if exists hassong;
+CREATE TABLE hassong
+	(upc 		CHAR(11) NOT NULL,
+    title 		CHAR(50) NOT NULL, -- how long should we allow song titles? I remember some pretty long ones back in the day
+    PRIMARY KEY (upc, title),
+    FOREIGN KEY (upc) REFERENCES item (upc));
 
-create table hassong
-	(upc 		CHAR(11) not null,
-    title 		CHAR(50) not null, -- how long should we allow song titles? I remember some pretty long ones back in the day
-    PRIMARY KEY (upc, title));
-    -- FOREIGN KEY (upc) REFERENCES item);
-
-drop table if exists purchase; -- changing name of order table to purchase
-
-create table purchase
-	(receiptid		CHAR(11) not null,
-    pdate 			CHAR(20) not null,
-    cid 			CHAR(11) not null,
-    cardnumber		CHAR(16) null,
-    expirydate		CHAR(20) null,
-    expecteddate 	CHAR(20) null,
-    delivereddate	CHAR(20) null,
-    PRIMARY KEY (receiptid));
-    -- FOREIGN KEY (cid) REFERENCES customer);
-
-drop table if exists purchaseitem;
-
-create table purchaseitem
-	(receiptid 		CHAR(11) not null,
-    upc				CHAR(11) not null,
-    quantity 		SMALLINT not NULL,
-    PRIMARY KEY (receiptid, upc));
-    -- FOREIGN KEY (receiptid) REFERENCES purchase,
-    -- FOREIGN KEY (upc) REFERENCES item);
-
-drop table if exists customer;
-
-create table customer
-	(cid 		CHAR(11) not null,
-    cpassword	CHAR(30) null,
-    cname 		CHAR(15) not null,
-    address 	CHAR(30) null,
-    phone		char(11) null,
+CREATE TABLE customer
+	(cid 		CHAR(11) NOT NULL,
+    cpassword	CHAR(30) NULL,
+    cname 		CHAR(15) NOT NULL,
+    address 	CHAR(30) NULL,
+    phone		CHAR(11) NULL,
     PRIMARY KEY (cid));
 
-drop table if exists returns;
+CREATE TABLE purchase
+	(receiptid		CHAR(11) NOT NULL,
+    pdate 			CHAR(20) NOT NULL,
+    cid 			CHAR(11) NOT NULL,
+    cardnumber		CHAR(16) NULL,
+    expirydate		CHAR(20) NULL,
+    expecteddate 	CHAR(20) NULL,
+    delivereddate	CHAR(20) NULL,
+    PRIMARY KEY (receiptid),
+    FOREIGN KEY (cid) REFERENCES customer (cid));
 
-create table returns
-	(retid		CHAR(11) not null,
-    rdate 		CHAR(20) not null,
-    receiptid 	CHAR(11) not null,
-    PRIMARY KEY (retid));
-    -- FOREIGN KEY (receiptid) references purchase);
+CREATE TABLE purchaseitem
+	(receiptid 		CHAR(11) NOT NULL,
+    upc				CHAR(11) NOT NULL,
+    quantity 		SMALLINT NOT NULL,
+    PRIMARY KEY (receiptid, upc),
+    FOREIGN KEY (receiptid) REFERENCES purchase (receiptid),
+    FOREIGN KEY (upc) REFERENCES item (upc));
 
-drop table if exists returnitem;
+CREATE TABLE returns
+	(retid		CHAR(11) NOT NULL,
+    rdate 		CHAR(20) NOT NULL,
+    receiptid 	CHAR(11) NOT NULL,
+    PRIMARY KEY (retid),
+    FOREIGN KEY (receiptid) REFERENCES purchase (receiptid));
 
-create table returnitem
-	(retid		CHAR(11) not null,
-    upc 		CHAR(11) not null,
-    quantity	smallint not null,
-    PRIMARY KEY (retid, upc));
-    -- FOREIGN KEY (upc) references item),
-    -- FOREIGN KEY (retid) references returns);
+CREATE TABLE returnitem
+	(retid		CHAR(11) NOT NULL,
+    upc 		CHAR(11) NOT NULL,
+    quantity	SMALLINT NOT NULL,
+    PRIMARY KEY (retid, upc),
+    FOREIGN KEY (upc) REFERENCES item (upc),
+    FOREIGN KEY (retid) REFERENCES returns (retid));
     
-commit;
+COMMIT;
     
     
 
