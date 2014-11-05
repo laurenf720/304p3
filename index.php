a58c82804e8f33cf5859cdda81b82354d937c40d
laurenf720 <laurenf720@gmail.com>
2014-11-05 11:33:28 -0800

    consolidating nav par prt 2

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title> AMS Website Search Page </title>
+		<link href="AMS.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+		<?php include 'emplogin.php';?>
+		<?php include 'navbar.php';?>
+
+		<p>index...</p>
+	</body>
+	<script src="ams.js"></script>
+</html>
