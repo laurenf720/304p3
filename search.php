fd625ddec9aaa3c697350629cf950ba8335ccab8
laurenf720 <laurenf720@gmail.com>
2014-11-05 12:14:41 -0800

    retrieves items and displays with buttons
    retrieves all items from the database and sorts it by title name.
    displays it in a table with buttons to View details and Add to Cart
    (buttons don’t do anything yet)

--- search.php
+++ search.php
@@ -5,10 +5,40 @@
 	</head>
 
 	<body>
-		<?php include 'emplogin.php';?>
 		<?php include 'navbar.php';?>
+		
+		<div id="wrap">
+			<h1>Search for an AMS Item</h1>
+			<p></p>
+		</div>
+		
+		<div align=center>
+			<table cellpadding=5 class="itemlist"><thead><th>Name</th><th>Type</th><th>Company</th><th>Price</th><th colspan=2>Actions</th></thead>
 
-		<p>Doing some searching...</p>
+			<?php
+				$connection = new mysqli("127.0.0.1", "root", "photon", "AMS");
+
+			    // Check that the connection was successful, otherwise exit
+			    if (mysqli_connect_errno()) {
+			        printf("Connect failed: %s\n", mysqli_connect_error());
+			        exit();
+			    }
+
+			    $result = $connection->query("select * from item order by title");
+			    while ($row=$result->fetch_assoc()){
+			    	echo "<tr>";
+			    	echo "<td>".$row['title']."</td>";
+			    	echo "<td>".$row['itype']."</td>";
+			    	echo "<td>".$row['company']."</td>";
+			    	echo "<td>".$row['price']."</td>";
+			    	echo "<td style=\"border-right: 1px black solid;\"><input type=\"submit\" name=\"submit\" border=0 value=\"View Details\">";
+			    	echo "<input type=\"submit\" name=\"submit\" border=0 value=\"Add to Cart\"></td>";
+			    	echo "</tr>";
+			    }
+
+			?>
+			</table>
+		</div>
 	</body>
 	<script src="ams.js"></script>
 </html>
