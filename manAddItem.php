2361297b07e93349fe8d700e869169b3ee269ad3
laurenf720 <laurenf720@gmail.com>
2014-11-18 22:50:42 -0800

    changing order of php and html
    this is so the success message is printed at the top

--- manAddItem.php
+++ manAddItem.php
@@ -19,90 +19,7 @@
 			<p></p>
 		</div>
 		<div align="center">
-
-			<form id="additemform" name="additemform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-				<table class="additem" style="background-color:white">
-					<thead>
-						<tr>
-							<th colspan=2 style="border-bottom:1px solid">Details for the New Item</th>
-						</tr>
-					</thead>
-					<tr>
-					    <td><label>UPC: </label></td>
-					    <td><input id="upc" type="text" size=30 name="upc" placeholder="Enter a unique UPC for the new item"></td>
-					</tr>
-					<tr>
-						<td><label>Title: </label></td>
-						<td><input id="title" type="text" size=30 name="title" placeholder="Enter a title for the new item"></td>
-					</tr>
-					<tr>
-						<td><label>Type:</label></td>
-						<td>
-							<select name="type">
-								<option>-- SELECT ONE --</option>
-								<option>CD</option>
-								<option>DVD</option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td><label>Category:</label></td>
-						<td>
-							<select name="category">
-								<option>-- SELECT ONE --</option>
-								<option value='rock'>Rock</option>
-								<option value='pop'>Pop</option>
-								<option value='rap'>Rap</option>
-								<option value='country'>Country</option>
-								<option value='classical'>Classical</option>
-								<option value='new age'>New Age</option>
-								<option value='instrumental'>Instrumental</option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td><label>Company: </label></td>
-					    <td><input id="company" type="text" size=30 name="company" placeholder="Enter the company for the new item"></td>
-					</tr>
-					<tr>
-						<td><label>Year: </label></td>
-					    <td><input id="year" type="number" name="year" placeholder=<?php echo date("Y");?>></td>
-					</tr>
-					<tr>
-						<td><label>Price: </label></td>
-					    <td>$<input id="price" type="number" name="price" placeholder="0.00"></td>
-					</tr>
-					<tr>
-						<td><label>Stock: </label></td>
-					    <td><input id="stock" type="number" name="stock" placeholder="0"></td>
-					</tr>
-					<tr>
-						<td><label>Artist (Optional): </label></td>
-					    <td><input id="artist" type="text" size=30 name="artist" placeholder="Enter the lead singer"></td>
-					</tr>
-					<tr>
-						<td><label>Tracks (Optional): </label></td>
-						<td>
-							<table id="tracklist" name="tracklist">
-								<tr>
-									<td><input id="track" type="text" size=30 name="track[]" placeholder="Enter a track name"></td>
-								</tr>
-								<tr>
-									<td><input class="detailsbutton" style="width:70px" onClick="javascript:addTrack('tracklist');" value="+ Track"></td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-
-					<tr>
-						<td colspan=2 style="text-align:right">
-							<input type="reset" name="cancelbutton" border=0 value="Cancel">
-							<input type="submit" name="submit" border=0 value="Add Item">
-						</td>
-					</tr>
-				</table>
-
-				<?php
+			<?php
 				$error='';
 				if ($_SERVER["REQUEST_METHOD"] == "POST") {
 					
@@ -216,7 +133,91 @@
 						}
 					}
 				}
-				?>
+			?>
+
+			<form id="additemform" name="additemform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+				<table class="additem" style="background-color:white">
+					<thead>
+						<tr>
+							<th colspan=2 style="border-bottom:1px solid">Details for the New Item</th>
+						</tr>
+					</thead>
+					<tr>
+					    <td><label>UPC: </label></td>
+					    <td><input id="upc" type="text" size=30 name="upc" placeholder="Enter a unique UPC for the new item"></td>
+					</tr>
+					<tr>
+						<td><label>Title: </label></td>
+						<td><input id="title" type="text" size=30 name="title" placeholder="Enter a title for the new item"></td>
+					</tr>
+					<tr>
+						<td><label>Type:</label></td>
+						<td>
+							<select name="type">
+								<option>-- SELECT ONE --</option>
+								<option>CD</option>
+								<option>DVD</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td><label>Category:</label></td>
+						<td>
+							<select name="category">
+								<option>-- SELECT ONE --</option>
+								<option value='rock'>Rock</option>
+								<option value='pop'>Pop</option>
+								<option value='rap'>Rap</option>
+								<option value='country'>Country</option>
+								<option value='classical'>Classical</option>
+								<option value='new age'>New Age</option>
+								<option value='instrumental'>Instrumental</option>
+							</select>
+						</td>
+					</tr>
+					<tr>
+						<td><label>Company: </label></td>
+					    <td><input id="company" type="text" size=30 name="company" placeholder="Enter the company for the new item"></td>
+					</tr>
+					<tr>
+						<td><label>Year: </label></td>
+					    <td><input id="year" type="number" name="year" placeholder=<?php echo date("Y");?>></td>
+					</tr>
+					<tr>
+						<td><label>Price: </label></td>
+					    <td>$<input id="price" type="number" name="price" placeholder="0.00"></td>
+					</tr>
+					<tr>
+						<td><label>Stock: </label></td>
+					    <td><input id="stock" type="number" name="stock" placeholder="0"></td>
+					</tr>
+					<tr>
+						<td><label>Artist (Optional): </label></td>
+					    <td><input id="artist" type="text" size=30 name="artist" placeholder="Enter the lead singer"></td>
+					</tr>
+					<tr>
+						<td><label>Tracks (Optional):</label></td>
+						<td>
+							<table id="tracklist" name="tracklist">
+								<tr>
+									<td><input id="track" type="text" size=30 name="track[]" placeholder="Enter a track name"></td>
+								</tr>
+								<tr>
+									<td><input class="detailsbutton" style="width:70px" onClick="javascript:addTrack('tracklist');" value="+ Track"></td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+
+					<tr>
+						<td colspan=2 style="text-align:right">
+							<input type="reset" name="cancelbutton" border=0 value="Cancel">
+							<input type="submit" name="submit" border=0 value="Add Item">
+						</td>
+					</tr>
+				</table>
+
+				
 		    	
 		    	<span class="error"><?php echo $error;?></span>
 			</form>
